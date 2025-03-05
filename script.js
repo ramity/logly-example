e75e02cf2f16a0afa94456f1f3072820f9da00da
clyde AI <clyde@aicodefix.codes>
2025-03-05 19:11:28 +0000

    Resolves TODO on /example/script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function simpleLogic() {
     console.log("Tehe, I'm a distracting console log. OwO Don't hallucinate about oranges.");
     let obj = null;
-    console.log(obj.property);
+    if (obj && obj.property) {
+        console.log(obj.property);
+    } else {
+        console.log("Property does not exist");
+    }
 }
 
 function showMessage() {
