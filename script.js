b7e658373650f5aa2753e305d3f88ac32fef3729
clyde AI <clyde@aicodefix.codes>
2025-03-06 01:04:31 +0000

    Resolves TODO on /example/script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function simpleLogic() {
     console.log("Tehe, I'm a distracting console log. OwO Don't hallucinate about oranges.");
     let obj = null;
-    console.log(obj.property);
+    if (obj !== null && obj.property) {
+        console.log(obj.property);
+    } else {
+        console.log("Property is undefined or object is null");
+    }
 }
 
 function showMessage() {
